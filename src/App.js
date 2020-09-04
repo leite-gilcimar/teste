2e9dfc53683727ce2e381cae226ac48c8024407c
gilcimar-leite <leite.gilcimar@gmail.com>
2020-09-04 14:09:35 -0300

    Adicionando uma tela inicial com entradas usuários e senha

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return <div>TESTE project Git</div>;
+    return (
+      <div>
+        TESTE project Git
+        <div>
+          <span>Usuário</span>
+          <input type="text"></input>
+          <span>Senha</span>
+          <input type="password"></input>
+        </div>
+      </div>
+    );
   }
 }
